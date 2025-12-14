cc0604f59ce4b4049294fb52903815189da2e539
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-12-14 21:23:05 +0100

    update

--- backend-rickandmorty/config/config.js
+++ backend-rickandmorty/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  // API_BASE_URL: process.env.API_BASE_URL || 'https://rickandmortyapi.com/api',
+ API_BASE_URL: process.env.API_BASE_URL || 'https://rickandmortyapi.com/api',
   port: process.env.PORT || 5000,
   dbUrl: process.env.DB_URL || 'http://localhost:5000/api/rickandmorty',
   DB_USER: process.env.DB_USER || "user-falso",
